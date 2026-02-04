5d5c47e08ad7dfb5654bf3ee2b79db49847ee8bd
Kunal Gupta <guptakunalx@gmail.com>
2026-02-04 12:49:46 +0530

    update baseUrl in axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: "http://task-management-backend-041c.onrender.com/api/v1",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
